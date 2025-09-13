1b4d63ab49e561a447e1acf8c49659218012ca3a
sonika <sonika.s87@kalvium.com>
2025-09-13 12:19:43 +0530

    intial commit

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express');
-const { resolve } = require('path');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
-const port = 3010;
+app.use(express.json());
 
-app.use(express.static('static'));
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/inventoryDB';
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+})
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+.catch((err) => {
+  console.log(`Error connecting to MongoDB: ${err}`);
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}5`);
 });
